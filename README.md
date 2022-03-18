4c6ab3e95ccdd9e4fdf9b81191a0377d42389ffd
Jacob Nguyen <jn@jacobello.com>
2022-03-18 17:26:20 -0600

    readme created

---
+++ README.md
@@ -0,0 +1,19 @@
+## Make Change Project
+
+### Week 1 Homework for Skill Distillery
+
+## Overview
+
+Cash register simulator
+TODO FINISH WRITING THIS!!!
+
+## Technologises/Skills Used
+* Java
+* Eclipse
+* Atom
+* Terminal
+
+TODO more
+
+## Lessons learned
+TODO write this 
--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -1,5 +1,12 @@
 package com.skilldistillery.makechange;
 
 public class CashRegister {
-
+	
+	public static void main(String[] args) {
+		double tender = 17.95;
+		double ten = tender % 2;
+		System.out.println((int)(ten));	
+		tender =  tender - (int)ten*10;
+		System.out.println(tender);
+	}
 }
