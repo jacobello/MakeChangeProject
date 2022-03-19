505108585afcdf0cc55fa181e980af39eb97b4a0
Jacob Nguyen <jn@jacobello.com>
2022-03-18 19:34:45 -0600

    added to  and

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -24,39 +24,39 @@ public class CashRegister {
 			System.out.println("Please give following change to customer:");
 		}
 		if (change >= 10) {
-			int ten = 1;
-			System.out.println((ten) + " ten dollar bill");
-			change = change - ten * 10;
+			double ten = change/10;
+			System.out.println((int)(ten) + " ten dollar bill(s)");
+			change = change - (int)ten * 10;
+		
 		}
 		if (change >= 5) {
-			double five = 1;
-			System.out.println((int) (five) + " five dollar bill");
+			double five = change/5;
+			System.out.println((int) (five) + " five dollar bill(s)");
 			change = change - (int) five * 5;
 		}
 		if (change >= 1) {
 			int one = (int) (change);
-			System.out.println(one + " one dollar bill");
+			System.out.println(one + " one dollar bill(s)");
 			change = change - (int) one * 1;
 		}
 		if (change >= 0.25) {
 			double quarter = change / 0.25;
-			System.out.println((int) (quarter) + " quarter");
+			System.out.println((int) (quarter) + " quarter(s)");
 			change = change - (int) quarter * 0.25;
 		}
 		if (change >= 0.10) {
 			double dime = change / 0.10;
-			System.out.println((int) (dime) + " dime");
+			System.out.println((int) (dime) + " dime(s)");
 			change = change - (int) dime * 0.1;
 		}
 		if (change >= 0.05) {
 			double nickel = change / 0.05;
-			System.out.println((int) (nickel) + " nickel");
+			System.out.println((int) (nickel) + " nickel(s)");
 			change = change - (int) nickel * 0.05;
 		}
 		if (change >= 0.009) {
 			double penny = change / 0.009;
-			System.out.println((int) (penny) + " penny");
+			System.out.println((int) (penny) + " penny(ies)");
 		}
-
 	}
 }
